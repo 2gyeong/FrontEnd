fad126fc6ca1a668737b64c4efa83108291a5fb6
Yigyeong <knk222@naver.com>
2023-01-13 19:01:41 +0900

    23.01.13.

---
+++ FrontEnd/Project/js/a.js
@@ -0,0 +1,17 @@
+function amount () {
+    var n1 = document.getElementById("p1-1-price").value;
+
+
+    var p1 = 9000*n1;
+
+    document.getElementById("p1-1-total").value = p1 ;
+
+    var totalNumber = parseInt(n1);
+
+    document.getElementById("totalNumer").value = totalNumer;
+
+    var totalPrice = p1 ;
+    document.getElementById("totalPrice").value = totalPrice;
+    
+}
+
