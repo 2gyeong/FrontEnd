e7c9fc3260cff410a4f42f72d743c696f132ebeb
Yigyeong <knk222@naver.com>
2023-01-16 19:30:21 +0900

    23.01.16.

--- FrontEnd/Project/js/amount.js
+++ FrontEnd/Project/js/amount.js
@@ -1,12 +1,31 @@
-function fnCalCount(type, ths){
-    var $input = $(ths).parents("td").find("input[name='pop_out']");
-    var tCount = Number($input.val());
-    var tEqCount = Number($(ths).parents("tr").find("td.bseq_ea").html());
+function count(type)  {
+    // 결과를 표시할 element
+    const resultElement = document.getElementById('c_result');
     
-    if(type=='p'){
-        if(tCount < tEqCount) $input.val(Number(tCount)+1);
-        
-    }else{
-        if(tCount >0) $input.val(Number(tCount)-1);    
-        }
-}
+    // 현재 화면에 표시된 값
+    let number = resultElement.innerText;
+    
+    // 더하기/빼기
+    if(type === 'plus') {
+      number = parseInt(number) + 1;
+    }else if(type === 'minus')  {
+      number = parseInt(number) - 1;
+    }
+    
+    // 결과 출력
+    resultElement.innerText = number;
+  }
+
+  Number.prototype.format = function(){
+    if(this==0) return 0;
+  
+    var reg = /(^[+-]?\d+)(\d{3})/;
+    var n = (this + '');
+  
+    while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
+  
+    return n;
+  };
+
+
+ 
